bb309c2610336cefa2accf41bd1fef38f869b526
slimm1 <tini.ramonda@gmail.com>
2024-01-04 22:21:55 +0100

    3rd try to fix portfolio bug + extra bug fix

--- assets/js/load.js
+++ assets/js/load.js
@@ -8,24 +8,14 @@ document.getElementById('translateButton').addEventListener('click', () => {
       window.location="index-en.html";
     }
     else{
-      window.location = "index.html#reloaded";
-    }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
-    if(page === 'index.html' && window.location.hash !== '#reloaded'){
-        window.location.hash = 'reloaded';
-        window.location.reload();
+      window.location = "index.html";
     }
 });
 
 document.getElementById('homeTitle').addEventListener('click', () => {
     var path = window.location.pathname;
     var page = path.split("/").pop();
-    console.log(page);
-    if(page == 'index.html'){
+    if(page == 'index.html' || page.length==0){
       window.location="index.html";
     }
     else{
